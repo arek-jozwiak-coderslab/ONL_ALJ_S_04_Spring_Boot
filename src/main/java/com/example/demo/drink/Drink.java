2989421f28e5856a07f2fa9d5ea55f360b8b0608
arcyro <arek@arcyro.pl>
2026-08-26 15:33:24 +0200

    thymeleaf new

--- src/main/java/com/example/demo/drink/Drink.java
+++ src/main/java/com/example/demo/drink/Drink.java
@@ -4,6 +4,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -14,6 +16,9 @@ public class Drink {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @NotBlank
     private String name;
+    @NotBlank
+    @Size(min = 5)
     private String description;
 }
--- src/main/java/com/example/demo/drink/DrinkController.java
+++ src/main/java/com/example/demo/drink/DrinkController.java
@@ -1,10 +1,15 @@
 package com.example.demo.drink;
 
 
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/drink")
@@ -21,4 +26,19 @@ public class DrinkController {
         model.addAttribute("drinks", repository.findAll());
         return "/drink/list";
     }
+    @GetMapping("/new")
+    public String add(Model model){
+        model.addAttribute("drink", new Drink());
+        return "/drink/add";
+    }
+    @PostMapping("/new")
+    public String create(@Valid Drink drink, BindingResult result, RedirectAttributes ra) {
+        if (result.hasErrors()) {
+            return "/drink/add";
+        }
+        repository.save(drink);
+        ra.addFlashAttribute("message", "Drink utworzony.");
+        return "redirect:/drink/list";
+    }
+
 }
